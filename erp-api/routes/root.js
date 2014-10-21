1c7cc76c556f31cd90db83ae0cf7ddd5026f2b24
Jarrett Meyer <jarrettmeyer@gmail.com>
2014-10-21 11:00:35 -0400

    Updates to package scripts.

--- erp-api/routes/root.js
+++ erp-api/routes/root.js
@@ -1,10 +1,12 @@
 var express = require("express");
+var config = require("erp-config");
 
 module.exports = function (router) {
 
   router.get("/", function (req, res) {
     res.json({
       ok: true,
+      api: config.apiBaseUrl,
       timestamp: Date.now()
     });
   });
